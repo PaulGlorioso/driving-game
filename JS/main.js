e5435f555d188e4cc8d028cae60d2db2d185d052
Paul Glorioso <goldcansio@gmail.com>
2021-12-31 15:28:59 -0800

    Added to event listener if spacebar is pressed, the car will move left. Added gps var to track car on which way its facing, and its x and y coordinates.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,12 @@
 var car = document.querySelector('.car');
+var xCounter = 1;
+var gps = {
+  facing: car.className,
+  x: 0,
+  y: 0
+};
+var vehicle = document.querySelector('.vehicle');
+
 document.addEventListener('keydown', function (event) {
   if (event.key === 'ArrowUp') {
     car.className = 'car up';
@@ -9,4 +17,13 @@ document.addEventListener('keydown', function (event) {
   } else if (event.key === 'ArrowDown') {
     car.className = 'car down';
   }
+  if (event.key === ' ') {
+    setInterval(moveCar, 16);
+  }
 });
+
+function moveCar() {
+  vehicle.style.left = xCounter + 'px';
+  gps.x = xCounter;
+  xCounter++;
+}
