9fb75558bc60da618a695e2fffbb60ca0e5e2c4d
Paul Glorioso <goldcansio@gmail.com>
2021-12-31 16:25:25 -0800

    Added to gps object an engine property to indicate if the car is moving, used that property in an if conditional to make the car move or make it stop.

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,12 @@ var car = document.querySelector('.car');
 var xCounter = 1;
 var gps = {
   facing: car.className,
+  engine: 'off',
   x: 0,
   y: 0
 };
 var vehicle = document.querySelector('.vehicle');
-
+var move;
 document.addEventListener('keydown', function (event) {
   if (event.key === 'ArrowUp') {
     car.className = 'car up';
@@ -18,7 +19,13 @@ document.addEventListener('keydown', function (event) {
     car.className = 'car down';
   }
   if (event.key === ' ') {
-    setInterval(moveCar, 16);
+    if (gps.engine === 'off') {
+      move = setInterval(moveCar, 16);
+      gps.engine = 'on';
+    } else if (gps.engine === 'on') {
+      clearInterval(move);
+      gps.engine = 'off';
+    }
   }
 });
 
